e33d3de0534580f29a2c9d4c181972a1bfd977a8
SureshPerera <avishkaruchirasuresh1@gmail.com>
2024-12-26 13:18:30 +0530

    Add project files.

---
+++ DICE ROLL GAME/AutoNumberGenerator.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+
+
+
+
+
+
+class AutoNumberGenerator
+{
+    Random rnd = new Random();
+    public int randomNum;
+   
+    public void RandomNumber()
+    {
+        for (int i = 0; i < 1; i++)
+        {
+            //genarate random number 
+             randomNum = rnd.Next(6);
+           
+        }
+    }
+}
---
+++ DICE ROLL GAME/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+AutoNumberGenerator generator = new AutoNumberGenerator();
+InputValidateChecker checker = new InputValidateChecker();
+
+generator.RandomNumber();
+int genarateNumber = generator.randomNum;
+//Console.WriteLine("number : "+ genarateNumber);
+checker.checkValidate(genarateNumber);
+
+Console.ReadLine();
